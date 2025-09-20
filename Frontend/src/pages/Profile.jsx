09622e66b0fb4ccd7ff5222a6ff153da185c533c
Manish-Yadav77 <manishkyadav969@gmail.com>
2025-09-20 23:05:57 +0530

    fixing smaller bugs

--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -42,6 +42,8 @@ const Profile = () => {
     }
   };
 
+  user?.createdAt && console.log(new Date(user.createdAt).toISOString().replace("Z", "+00:00"))
+
   return (
     <div className="min-h-screen bg-gray-50 pt-16">
       <div className="max-w-4xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
